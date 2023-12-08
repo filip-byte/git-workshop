b905f3d53526d9757f36e5c451d5bb5b21de9cc2
Simizzle <85163420+Simizzle@users.noreply.github.com>
2023-12-08 16:04:16 +0000

    inital commit

---
+++ src/main/java/Advanced.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class Advanced {
+
+    public static void main(String[] args) {
+
+        String[] wordList = WordList.wordList;
+
+        // DO NOT CHANGE ANY CODE ABOVE THIS LINE
+
+        System.out.println(Arrays.toString(wordList));
+
+    }
+}
